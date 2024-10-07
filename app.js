6580e295fedefaa83cf0949e192938af847b83cc
MiguelCarreraE <90919223+MiguelCarreraE@users.noreply.github.com>
2024-10-07 16:03:22 -0600

    Hotfix name correcto html

--- app.js
+++ app.js
@@ -42,12 +42,12 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Botón Ingresar fue clickeado');  // Para verificar si el evento se activa
 
         if (usuario === 'fernandoorivas' && contrasena === 'DiseñoSoftware') {
-            console.log('Credenciales correctas');  // Para verificar si pasa por aquí
-            window.location.href = 'correcto.html';  // Cambia 'correcto.html' por la página a la que quieras redirigir
+            console.log('Credenciales correctas'); 
+            window.location.href = 'home.html'; 
         } else {
             loginResultado.innerHTML = '<p>Usuario o contraseña incorrectos. Inténtalo de nuevo.</p>';
             loginResultado.style.color = 'red';
-            console.log('Credenciales incorrectas');  // Para verificar si pasa por aquí
+            console.log('Credenciales incorrectas');  
         }
     });
 });
